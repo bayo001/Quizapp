9c1849a67811738514bfd98e664ada88eb1c16a5
bayo <abdulwahababdullahi064@gmail.com>
2023-09-03 11:23:40 -0700

    first commit

---
+++ js/question.js
@@ -0,0 +1,552 @@
+let ques = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What is making web Standard?",
+        answer: "The World Wide Web Consortium" ,
+        options: [
+            "Google",
+            "Microsoft",
+            "Mozilla",
+            "The World Wide Web Consortium"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Choose the correct HTML element for the largest heading:",
+        answer:  "<h1>",
+        options: [
+            "Hyper Text Markup Language2",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What is the correct HTML element for inserting a line break?",
+        answer:  "<br>",
+        options: [
+            "Hyper Text Markup Language3",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What is the correct HTML for adding a background color?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Markup Language4",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 6,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup Language5",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 7,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup Language6",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 8,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup Language7",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 9,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup Language8",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 10,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup Language9",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 11,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup1 Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 12,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup2 Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 13,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup3 Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 14,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup4 Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 15,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup5 Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 16,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup6 Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 17,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup7 Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 18,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup8 Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 19,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup9 Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 20,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool1 Markup Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 21,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool2 Markup Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 22,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool3 Markup Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 23,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool4 Markup Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 24,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool5 Markup Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 25,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool6 Markup Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 26,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool7 Markup Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 27,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool8 Markup Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 28,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool9 Markup Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 29,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup Language",
+            "Hyper Text1 Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 30,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup Language",
+            "Hyper Text2 Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 31,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup Language",
+            "Hyper Text3 Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 32,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup Language",
+            "Hyper Text Markup Language",
+            "Home Tool4 Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 33,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlin5ks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 34,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup Language",
+            "Hyper Text Mark6up Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 35,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup 7Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 36,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup 8Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 37,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup Language",
+            "Hyper Text Markup Language",
+            "Home Tool 9Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 38,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hy1perlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 39,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hy2perlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 40,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hy3perlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 41,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hy4per Tool Markup Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 42,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hy5perlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 43,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "H6yperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 44,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "7Hyper Tool Markup Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 45,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup Language",
+            "Hyper Text Markup Language",
+            "8Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 46,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup Language",
+            "Hyper Text Markup Language",
+            "9Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 47,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "1Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 48,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup Language",
+            "Hyper Text Markup Language",
+            "Ho2me Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 49,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup Language",
+            "Hyper Text Markup Language",
+            "Home To3ol Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 50,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperli4nks and Text Markup Language"
+        ]
+    },
+    ];
